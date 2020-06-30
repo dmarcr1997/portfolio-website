35c4a21ac97a15caa9d6443b240a216a7d0c48be
dmarc <dmarcr1997@gmail.com>
2020-06-29 20:44:32 -0500

    fixed

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () =>{
             <p>I recieved my degree in Computer Science in 2019 at Grayson County college. During my time there, I studied Physics, Math, Calculus, Computer Assembly, C++, and various other subjects.</p> 
             <p>Upon completing my degree I decided to start the Flatiron Bootcamp. After six months of work, I finished the program and am now experienced in Ruby on Rails, JavaScript-based programming, and React.</p>
             <p> I possess strong skills in team-building and project management which I gained while working at UPS and as a supervisor at Kohls. In conclusion I believe I would be a great fit at any company that would have me.</p>
-            <p> Thank you taking the time to look at my portfolio; contact me via email for more information. I look forward to hearing from you.</p>
+            <p> Thank you for taking the time to look at my portfolio; contact me via email for more information. I look forward to hearing from you.</p>
         </div>)
 }
 
