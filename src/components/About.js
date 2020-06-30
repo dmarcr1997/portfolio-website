351408b62ceb721fc994aa502de04ce430cf10bf
dmarc <dmarcr1997@gmail.com>
2020-06-29 20:00:11 -0500

    changed about

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,12 @@ const About = () =>{
     return(
         <div>
             <h1>About</h1>
-            <p>Hi my name is Damon Rocha and I am 23 years old. I am a passionate tinkerer, and love to build things with my hands. Such as software, robots, basic electronics, arrowheads, knives, and much more!</p>
+            <p>Hi my name is Damon Rocha and I am 23 years old. I am a passionate tinkerer and love to build things with my hands such as: software, robots, basic electronics, arrowheads, knives, and much more!</p>
             <p>I have been coding for about three years now, and can say I have thoroughly enjoyed every second of it.</p>
-            <p>I recieved my degree in Computer Science in 2019 at Grayson County college. While here I studied Physics, Math, Calculus, Computer Assembly, C++, and various other subjects.</p> 
+            <p>I recieved my degree in Computer Science in 2019 at Grayson County college. During my time there, I studied Physics, Math, Calculus, Computer Assembly, C++, and various other subjects.</p> 
             <p>Upon completing my degree I decided to start the Flatiron Bootcamp. After six months of work, I finished the program and am now experienced in Ruby on Rails, JavaScript-based programming, and React.</p>
-            <p>In conclusion I would be a great fit at any company that would have me. I possess strong skills in team-building and project management which I gained while working at UPS and as a supervisor at Kohls.</p>
-            <p> So if this all sounds good sned me an email. Thank you</p>
+            <p> I possess strong skills in team-building and project management which I gained while working at UPS and as a supervisor at Kohls. In conclusion I believe I would be a great fit at any company that would have me.</p>
+            <p> Thank you taking the time to look at my portfolio; contact me via email for more information. I look forward to hearing from you.</p>
         </div>)
 }
 
